06ba09a841293515e52a7ce39de4d116ab294e50
Mickael Dumand <mickael@getluko.com>
2022-07-26 17:30:37 +0200

    fix: bin node env

--- src/execAndroidRollout.ts
+++ src/execAndroidRollout.ts
@@ -1,6 +1,7 @@
+#!/usr/bin/env node
+// npx ts-node -T scripts/google/execAndroidRollout.ts com.package.name production ./api-google-service_account.json
 import { incrementAndroidRollout } from "./incrementAndroidRollout";
 
-// npx ts-node -T scripts/google/execAndroidRollout.ts com.package.name production ./api-google-service_account.json
 const packageName: string = process.argv[2];
 const track: string = process.argv[3];
 const keyFile: string = process.argv[4];
